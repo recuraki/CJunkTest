18446c4e6e426a5d772a1563f02ef2b43ed163af
Akira Kanai <kanai@gw3.mon.mfeed.ad.jp>
2014-01-14 19:35:11 +0900

    add timezone name

--- system/timezone.c
+++ system/timezone.c
@@ -11,4 +11,5 @@ main()
  printf("%4d-%02d-%02dT%02d:%02d:%02d%+02.2d:00 \n ", 
         (tptr->tm_year)+1900, tptr->tm_mon+1, tptr->tm_mday,
          tptr->tm_hour, tptr->tm_min, tptr->tm_sec, - timezone / 3600);
+ printf("[%s]\n", tzname[0]);
 }
